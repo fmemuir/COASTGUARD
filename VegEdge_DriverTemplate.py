e080379a11f10d654beffd0040013d40e6a1a5de
fmemuir <freyamuir@me.com>
2023-09-01 17:16:32 +0100

    Big example driver file tidy

--- VegEdge_DriverTemplate.py
+++ VegEdge_DriverTemplate.py
@@ -5,15 +5,13 @@ Created on Thu Sep 15 13:08:30 2022
 
 @author: fmuir
 """
-#!/usr/bin/env python
-# coding: utf-8
+
 
 #%% Imports and Initialisation
 
 
 import os
 import glob
-import numpy as np
 import pickle
 import warnings
 warnings.filterwarnings("ignore")
@@ -30,64 +28,75 @@ ee.Initialize()
 
 
 
-#%% Define AOI using coordinates of a rectangle
+#%% EDIT ME: Requirements
 
+# Define desired coordinate projections
 projection_epsg = 27700 # OSGB 1936
 image_epsg = 32630 # UTM Zone 30N
 
+# Define AOI using coordinates of a rectangle
 # The points represent the corners of a bounding box that go around your site
 sitename = 'SITENAME'
 lonmin, lonmax = -2.84869, -2.79878
 latmin, latmax = 56.32641, 56.39814
 
+# Date range
+dates = ['2021-05-01', '2021-07-02']
+
+# Satellite missions
+# Input a list of containing any/all of 'L5', 'L7', 'L8', 'L9', 'S2', 'PSScene4Band'
+# L5: 1984-2013; L7: 1999-2017 (SLC error from 2003); L8: 2013-present; S2: 2014-present; L9: 2021-present
+sat_list = ['L5','L8','S2']
+
+# Cloud threshold for screening out cloudy imagery (0.5 or 50% recommended)
+cloud_thresh = 0.5
+
+# Extract shoreline (wet-dry boundary) as well as veg edge
+wetdry = True
+
+# Reference shoreline/veg line shapefile name (should be stored in a folder called referenceLines in Data)
+referenceLineShp = 'SITENAME_refLine.shp'
+# Maximum amount in metres by which to buffer the reference line for capturing veg edges within
+max_dist_ref = 150
+
+
+#%% Set Up Site Directory
+# Directory where the data will be stored
+filepath = Toolbox.CreateFileStructure(sitename, sat_list)
+
 # Return AOI after checking coords and saving folium map HTML in sitename directory
 polygon, point = Toolbox.AOI(lonmin, lonmax, latmin, latmax, sitename, image_epsg)
 
-# it's recommended to convert the polygon to the smallest rectangle (sides parallel to coordinate axes)       
+# It's recommended to convert the polygon to the smallest rectangle (sides parallel to coordinate axes)       
 polygon = Toolbox.smallest_rectangle(polygon)
 
-#%% Image Settings
+#%% Compile Input Settings for Imagery
 
-# directory where the data will be stored
-filepath = os.path.join(os.getcwd(), 'Data')
-if os.path.isdir(filepath) is False:
-    os.mkdir(filepath)
-
-# date range
-dates = ['2021-05-01', '2021-07-02']
 if len(dates)>2:
     daterange='no'
 else:
     daterange='yes'
 years = list(Toolbox.daterange(datetime.strptime(dates[0],'%Y-%m-%d'), datetime.strptime(dates[-1],'%Y-%m-%d')))
 
-# satellite missions
-# Input a list of containing any/all of 'L5', 'L7', 'L8', 'L9', 'S2', 'PSScene4Band'
-# L5: 1984-2013; L7: 1999-2017 (SLC error from 2003); L8: 2013-present; S2: 2014-present; L9: 2021-present
-sat_list = ['L5','L8','S2']
-
-# put all the inputs into a dictionnary
+# Put all the inputs into a dictionnary
 inputs = {'polygon': polygon, 'dates': dates, 'daterange':daterange, 'sat_list': sat_list, 'sitename': sitename, 'filepath':filepath}
 
-direc = os.path.join(filepath, sitename)
 
-if os.path.isdir(direc) is False:
-    os.mkdir(direc)
- 
-    
 #%% Image Retrieval
 
-# before downloading the images, check how many images are available for your inputs
+# Before downloading the images, check how many images are available for your inputs
 Download.check_images_available(inputs)
 
 
 #%% Image Download
 
+# Make the metadata
 Sat = Toolbox.image_retrieval(inputs)
 metadata = Toolbox.metadata_collection(sat_list, Sat, filepath, sitename)
 
 
 #%% Vegetation Edge Settings
+# ONLY EDIT IF ADJUSTMENTS ARE NEEDED
 
 BasePath = 'Data/' + sitename + '/Veglines'
 
@@ -96,9 +105,9 @@ if os.path.isdir(BasePath) is False:
 
 settings = {
     # general parameters:
-    'cloud_thresh': 0.5,        # threshold on maximum cloud cover
+    'cloud_thresh': cloud_thresh,        # threshold on maximum cloud cover
     'output_epsg': image_epsg,     # epsg code of spatial reference system desired for the output   
-    'wetdry':True,              # extract wet-dry boundary as well as veg
+    'wetdry': wetdry,              # extract wet-dry boundary as well as veg
     # quality control:
     'check_detection': True,    # if True, shows each shoreline detection to the user for validation
     'adjust_detection': False,  # if True, allows user to adjust the postion of each shoreline by changing the threhold
@@ -116,43 +125,38 @@ settings = {
 
 
 #%% Compute Tides from FES2014
-tidepath = "/path/to/FES2014/files/aviso-fes/data/fes2014"
+tidepath = "/path/to/your/FES2014/files/aviso-fes/data/fes2014"
 daterange = dates
-tidelatlon = [-2.79878,latmax-(2/latmin)] # seaward edge, halfway between S and N
+tidelatlon = [lonmax,latmax-(2/latmin)] # seaward edge, halfway between S and N
 Toolbox.ComputeTides(settings,tidepath,daterange,tidelatlon) 
     
 #%% Vegetation Edge Reference Line Load-In
 
-"""
-OPTION 2: Load in coordinates of reference line shapefile and format for use in
-the veg extraction.
-"""
-
-#referenceLineShp = os.path.join(inputs['filepath'], sitename,'StAndrews_refLine.shp')
-referenceLineShp = os.path.join(inputs['filepath'], 'SITE_refLine.shp')
-referenceLine, ref_epsg = Toolbox.ProcessRefline(referenceLineShp,settings)
+referenceLinePath = os.path.join(inputs['filepath'], 'referenceLines', referenceLineShp)
+referenceLine, ref_epsg = Toolbox.ProcessRefline(referenceLinePath,settings)
 
 settings['reference_shoreline'] = referenceLine
 settings['ref_epsg'] = ref_epsg
 # Distance to buffer reference line by (this is in metres)
-settings['max_dist_ref'] = 150
+settings['max_dist_ref'] = max_dist_ref
 
 
 #%% Vegetation Line Extraction
+"""
+OPTION 1: Run extraction tool and return output veg edges as a dictionary of lines
+"""
 
-"""
-OPTION 1: Run extraction tool and return output dates, lines, filenames and 
-image properties.
-"""
-#get_ipython().run_line_magic('matplotlib', 'qt')
-clf_model = 'Aberdeen_MLPClassifier_Veg_S2.pkl'
+# Choose which ANN classifier to use (default is uncommented)
+# clf_model = 'MLPClassifier_Veg_L8S2.pkl'
+# clf_model = 'MLPClassifier_Veg_PSScene.pkl'
+clf_model = 'MLPClassifier_Veg_L5L8S2.pkl' 
+
 output, output_latlon, output_proj = VegetationLine.extract_veglines(metadata, settings, polygon, dates)
 
 
 #%% Vegetation Line Extraction Load-In
-
 """
-OPTION 2: Load in pre-existing output dates, lines, filenames and image properties.
+OPTION 2: Load in pre-existing outputs
 """
 
 SiteFilepath = os.path.join(inputs['filepath'], sitename)
@@ -164,28 +168,35 @@ with open(os.path.join(SiteFilepath, sitename + '_output_proj.pkl'), 'rb') as f:
     output_proj = pickle.load(f)
     
 
-#%% remove duplicate date lines 
+#%% Remove Duplicate Lines
+# (images taken on the same date by the same satellite)
 
-output = Toolbox.remove_duplicates(output) # removes duplicates (images taken on the same date by the same satellite)
+output = Toolbox.remove_duplicates(output) 
 output_latlon = Toolbox.remove_duplicates(output_latlon)
 output_proj = Toolbox.remove_duplicates(output_proj)
 
-#%% Save the veglines as shapefiles locally
+#%% Save Veglines as Local Shapefiles
+
 # Save output veglines 
 Toolbox.SaveConvShapefiles(output, BasePath, sitename, settings['projection_epsg'])
+# Save output shorelines if they were generated
 if settings['wetdry'] == True:
     Toolbox.SaveConvShapefiles_Water(output, BasePath, sitename, settings['projection_epsg'])
 
-#%% Create GIF of satellite images and related shorelines
 
-Plotting.SatGIF(metadata,settings,output)
+#%% EDIT ME: Define Settings for Cross-shore Transects
 
-#%% Create Transects
 SmoothingWindowSize = 21 
 NoSmooths = 100
 TransectSpacing = 10
 DistanceInland = 100
 DistanceOffshore = 350
+
+# provide average beach slope for site, for calculating corrected beach widths
+beachslope = 0.02 
+
+
+#%% Create Cross-shore Transects
 
 BasePath = 'Data/' + sitename + '/veglines'
 VeglineShp = glob.glob(BasePath+'/*veglines.shp')
@@ -195,80 +206,112 @@ WaterlineGDF = gpd.read_file(WaterlineShp[0])
 # Produces Transects for the reference line
 TransectSpec =  os.path.join(BasePath, sitename+'_Transects.shp')
 
+# If transects already exist, load them in
 if os.path.isfile(TransectSpec) is False:
-    TransectGDF = Transects.ProduceTransects(SmoothingWindowSize, NoSmooths, TransectSpacing, DistanceInland, DistanceOffshore, settings['output_epsg'], sitename, BasePath, referenceLineShp)
+    TransectGDF = Transects.ProduceTransects(SmoothingWindowSize, NoSmooths, TransectSpacing, DistanceInland, DistanceOffshore, settings['output_epsg'], sitename, BasePath, referenceLinePath)
 else:
     print('Transects already exist and were loaded')
     TransectGDF = gpd.read_file(TransectSpec)
 
-#%% Create (or load) intersections with sat and validation lines per transect
+#%% Transect-Veg Intersections
+# Create (or load) intersections with satellite and validation lines per transect
 
-if os.path.isfile(os.path.join(filepath, sitename, sitename + '_transect_intersects.pkl')):
-    print('TransectDict exists and was loaded')
-    with open(os.path.join(filepath , sitename, sitename + '_transect_intersects.pkl'), 'rb') as f:
-        TransectDict, TransectInterGDF = pickle.load(f)
+if os.path.isfile(os.path.join(filepath, sitename, 'intersections', sitename + '_transect_intersects.pkl')):
+    print('Transect Intersect GDF exists and was loaded')
+    with open(os.path.join
+              (filepath , sitename, 'intersections', sitename + '_transect_intersects.pkl'), 'rb') as f:
+        TransectInterGDF = pickle.load(f)
 else:
     # Get intersections
-    TransectDict = Transects.GetIntersections(BasePath, TransectGDF, VeglineGDF)
+    TransectInterGDF = Transects.GetIntersections(BasePath, TransectGDF, VeglineGDF)
     # Save newly intersected transects as shapefile
-    TransectInterGDF = Transects.SaveIntersections(TransectDict, VeglineGDF, BasePath, sitename, settings['projection_epsg'])
+    TransectInterGDF = Transects.SaveIntersections(TransectInterGDF, VeglineGDF, BasePath, sitename)
     # Repopulate dict with intersection distances along transects normalised to transect midpoints
-    TransectDict = Transects.CalculateChanges(TransectDict,TransectInterGDF)
+    TransectInterGDF = Transects.CalculateChanges(TransectInterGDF)
+    
+    with open(os.path.join(filepath, sitename, 'intersections', sitename + '_transect_intersects.pkl'), 'wb') as f:
+        pickle.dump(TransectInterGDF, f)
+        
+#%% Transect-Water Intersections
+if os.path.isfile(os.path.join(filepath, sitename, 'intersections', sitename + '_transect_water_intersects.pkl')):
+    print('Transect Intersect + Water GDF exists and was loaded')
+    with open(os.path.join
+              (filepath , sitename, 'intersections', sitename + '_transect_water_intersects.pkl'), 'rb') as f:
+        TransectInterGDFWater = pickle.load(f)
+else:        
     if settings['wetdry'] == True:
-        beachslope = 0.02 # provide average beach slope for site
-        TransectDict = Transects.GetBeachWidth(BasePath, TransectGDF, TransectDict, WaterlineGDF, settings, output, beachslope)  
-        TransectInterGDF = Transects.SaveWaterIntersections(TransectDict, WaterlineGDF, TransectInterGDF, BasePath, sitename, settings['projection_epsg'])
+        TransectInterGDFWater = Transects.GetBeachWidth(BasePath, TransectGDF, TransectInterGDF, WaterlineGDF, settings, output, beachslope)  
+        TransectInterGDFWater = Transects.SaveWaterIntersections(TransectInterGDFWater, WaterlineGDF,  BasePath, sitename, settings['projection_epsg'])
     
-    with open(os.path.join(filepath , sitename, sitename + '_transect_intersects.pkl'), 'wb') as f:
-        pickle.dump([TransectDict,TransectInterGDF], f)
+    with open(os.path.join(filepath, sitename, 'intersections', sitename + '_transect_water_intersects.pkl'), 'wb') as f:
+        pickle.dump(TransectInterGDFWater, f)
+
+#%% Transect-Topo Intersections
 
 
-#%% VALIDATION
+#%% Transect-Waves Intersections
 
-# Name of date column in validation shapefile (case sensitive!) 
-DatesCol = 'Date'
-ValidationShp = './Validation/StAndrews_Veg_Edge_combined_2007_2022_singlepart.shp'
-validpath = os.path.join(os.getcwd(), 'Data', sitename, 'validation')
 
-if os.path.isfile(os.path.join(validpath, sitename + '_valid_dict.pkl')):
-    print('ValidDict exists and was loaded')
-    with open(os.path.join(validpath, sitename + '_valid_dict.pkl'), 'rb') as f:
-        ValidDict = pickle.load(f)
-else:
-    ValidDict = Transects.ValidateSatIntersects(sitename, ValidationShp, DatesCol, TransectGDF, TransectDict)
-    with open(os.path.join(validpath, sitename + '_valid_dict.pkl'), 'wb') as f:
-        pickle.dump(ValidDict, f)
+#%% Timeseries Plotting
 
-#%%
-ValidDict = Transects.ValidateSatIntersects(ValidationShp, DatesCol, TransectGDF, TransectDict)
-#%% Validation Plots
-TransectIDList= [(0,1741)]
-
-for TransectIDs in TransectIDList:
-    PlotTitle = 'Accuracy of Transects ' + str(TransectIDs[0]) + ' to ' + str(TransectIDs[1])
-    PlottingSeaborn.SatViolin(sitename,VeglineShp[0],'dates',ValidDict,TransectIDs, PlotTitle)
-    
-#%% Quantify errors between validation and satellite derived lines
-TransectIDList = [(595,711),(726,889),(972,1140),(1141,1297)]
-for TransectIDs in TransectIDList:
-    Toolbox.QuantifyErrors(sitename, VeglineShp[0],'dates',ValidDict,TransectIDs)
-    
-    
-    
-#%% TIMESERIES PLOTTING
-
-# Select transect ID to plot
-TransectIDs = [1575]
+# EDIT ME: Select transect ID to plot
+TransectIDs = [[1575,309]]
 for TransectID in TransectIDs:
-    DateRange = [0,len(TransectDict['dates'][TransectID])]
+    DateRange = [0,len(TransectInterGDF['dates'][TransectID])]
     # Plot timeseries of cross-shore veg position
-    Plotting.VegTimeseries(sitename, TransectDict, TransectID, DateRange)
+    Plotting.VegTimeseries(sitename, TransectInterGDF, TransectID, DateRange)
+    # If plotting veg and water lines together
+    # Plotting.VegWaterTimeseries(sitename, TransectInterGDF, TransectID, Hemisphere='N')
+
     
-#%%
+#%% Beach Width Plotting
 
 # Select transect ID to plot
 TransectIDs = [180,1650]
 for TransectID in TransectIDs:
     # Plot timeseries of cross-shore width between water edge and veg edge 
-    DateRange = [0,len(TransectDict['dates'][TransectID])]
-    Plotting.WidthTimeseries(sitename, TransectDict, TransectID, DateRange)
+    DateRange = [0,len(TransectInterGDF['dates'][TransectID])]
+    Plotting.WidthTimeseries(sitename, TransectInterGDF, TransectID, DateRange)
+
+
+#%% EDIT ME: Validation Settings
+# Most likely you won't need to validate your lines, but if you do, edit these parameters
+
+# Name of date column in validation edges shapefile (case sensitive!) 
+DatesCol = 'Date'
+ValidationShp = './Validation/StAndrews_Veg_Edge_combined_2007_2022_singlepart.shp'
+
+#%% Satellite Edges Validation
+
+validpath = os.path.join(os.getcwd(), 'Data', sitename, 'validation')
+
+if os.path.isfile(os.path.join(validpath, sitename + '_valid_intersects.pkl')):
+    print('ValidDict exists and was loaded')
+    with open(os.path.join(validpath, sitename + '_valid_intersects.pkl'), 'rb') as f:
+        ValidInterGDF = pickle.load(f)
+else:
+    ValidInterGDF = Transects.ValidateSatIntersects(sitename, ValidationShp, DatesCol, TransectGDF, TransectInterGDF)
+    with open(os.path.join(validpath, sitename + '_valid_intersects.pkl'), 'wb') as f:
+        pickle.dump(ValidInterGDF, f)
+
+
+#%% Validation Plots
+# EDIT ME: List of transect ID tuples (startID, finishID)
+TransectIDList = [(0,1741)]
+
+for TransectIDs in TransectIDList:
+    PlotTitle = 'Accuracy of Transects ' + str(TransectIDs[0]) + ' to ' + str(TransectIDs[1])
+    PlottingSeaborn.SatViolin(sitename,VeglineGDF,'dates',ValidInterGDF,TransectIDs, PlotTitle)
+    PlottingSeaborn.SatPDF(sitename,VeglineGDF,'dates',ValidInterGDF,TransectIDs, PlotTitle)
+    Plotting.SatRegress(sitename,VeglineGDF,'dates',ValidInterGDF,TransectIDs, PlotTitle)
+
+    
+#%% Quantify errors between validation and satellite derived lines
+# EDIT ME: List of transect ID tuples (startID, finishID
+TransectIDList = [(595,711),(726,889),(972,1140),(1141,1297)]
+
+for TransectIDs in TransectIDList:
+    Toolbox.QuantifyErrors(sitename, VeglineGDF,'dates',ValidInterGDF,TransectIDs)
+    
+
+    
