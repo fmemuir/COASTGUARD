24af9352d409b2c32e061e15734b5a558f19d129
fmemuir <freyamuir@me.com>
2023-10-24 13:45:47 +0100

    skimage morphology no longer has 'inplace' as arg

--- Toolshed/Image_Processing.py
+++ Toolshed/Image_Processing.py
@@ -855,13 +855,13 @@ def create_cloud_mask(im_QA, satname, cloud_mask_issue):
     # remove cloud pixels that form very thin features. These are beach or swash pixels that are
     # erroneously identified as clouds by the CFMASK algorithm applied to the images by the USGS.
     if sum(sum(cloud_mask)) > 0 and sum(sum(~cloud_mask)) > 0:
-        morphology.remove_small_objects(cloud_mask, min_size=10, connectivity=1, in_place=True)
+        cloud_mask = morphology.remove_small_objects(cloud_mask, min_size=10, connectivity=1)
 
         if cloud_mask_issue:
             elem = morphology.square(3) # use a square of width 3 pixels
             cloud_mask = morphology.binary_opening(cloud_mask,elem) # perform image opening
             # remove objects with less than 25 connected pixels
-            morphology.remove_small_objects(cloud_mask, min_size=25, connectivity=1, in_place=True)
+            cloud_mask = morphology.remove_small_objects(cloud_mask, min_size=25, connectivity=1)
 
     return cloud_mask
 
