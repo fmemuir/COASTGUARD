2f7028acbc3865dd3dec134415c96e5122cd2a57
fmemuir <freyamuir@me.com>
2024-11-26 15:41:47 +0000

    Documentation

--- CoastLearn_PredictionDriver.py
+++ CoastLearn_PredictionDriver.py
@@ -7,25 +7,6 @@ Created on Thu Jun 13 10:51:41 2024
 """
 
 import os
-import pickle
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.model_selection import train_test_split
-
-# from sklearn.utils.class_weight import compute_class_weight
-# from imblearn.over_sampling import SMOTE
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import Input
-from tensorflow.keras.layers import GRU, Dense, Dropout
-from tensorflow.keras.optimizers import Adam
-from imblearn.over_sampling import SMOTE
-from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
-
 from Toolshed import Predictions
 
 # Only use tensorflow in CPU mode
@@ -34,7 +15,7 @@ tf.config.set_visible_devices([],'GPU')
 # %load_ext tensorboard
 # import tensorboard
 
-#%%
+#%% Load Transect Data
 # Name of site to save directory and files under
 sitename = 'StAndrewsEastS2Full'
 filepath = os.path.join(os.getcwd(), 'Data')
@@ -42,27 +23,29 @@ filepath = os.path.join(os.getcwd(), 'Data')
 # Load in transect data with coastal change variables
 TransectInterGDF, TransectInterGDFWater, TransectInterGDFTopo, TransectInterGDFWave = Predictions.LoadIntersections(filepath, sitename)
 
-#%% Compile relevant coastal change metrics into one dataframe
+#%% Compile Transect Data
+# Compile relevant coastal change metrics into one dataframe
 CoastalDF = Predictions.CompileTransectData(TransectInterGDF, TransectInterGDFWater, TransectInterGDFTopo, TransectInterGDFWave)
 
-#%% Subset and interpolate timeseries to match up to same dates
+#%% Interpolate
+# Subset and interpolate timeseries to match up to same dates
 # TransectIDs = [271]
 TransectIDs = [1325]
 
 for Tr in TransectIDs:
     TransectDF = Predictions.InterpVEWL(CoastalDF, Tr)
     
+#%% Prepare Training Data
+PredDict = Predictions.PrepData(TransectDF, MLabels=['test1'], TestSizes=[0.2], TSteps=[30])
 
-#%%
-VarDF = Predictions.Cluster(TransectDF, ValPlots=True)
+#%% Compile the Recurrent Neural Network 
+# with desired number of epochs and batch size (per model run)
+PredDict = Predictions.CompileRNN(PredDict, epochSizes=[100], batchSizes=[32])
 
-#%%
-PredDict = Predictions.PrepData(TransectDF, ['test1','test2'], [0.2,0.2], [30,90])
-
-#%%
-PredDict = Predictions.CompileRNN(PredDict)
-
-#%%
+#%% Train Neural Network
+# FIlepath and sitename are used to save pickle file of model runs under
 PredDict = Predictions.TrainRNN(PredDict,filepath,sitename)
 
+#%% Cluster Past Observations
+# VarDF = Predictions.Cluster(TransectDF, ValPlots=True)
 
